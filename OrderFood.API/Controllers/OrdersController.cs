aa3d8e705d2c562e2fc7e3cace1d5468e4e7f18e
KienDev <ngotrungkien2k2@gmail.com>
2023-11-04 23:46:42 +0700

    CRUD order admin

---
+++ OrderFood.API/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+﻿using OrderFood.BL;
+using OrderFood.BL.OrderBL;
+using OrderFood.BL.RoleBL;
+using OrderFood.Common.Models;
+
+namespace OrderFood.API.Controllers
+{
+    public class OrdersController : BaseController<Order>
+    {
+        private IOrderBL _orderBL;
+
+        public OrdersController(IOrderBL orderBL) : base(orderBL)
+        {
+            _orderBL = orderBL;
+        }
+    }
+}
