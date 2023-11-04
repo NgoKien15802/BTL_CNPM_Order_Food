6f500e58f2035a6c9062b9ba3aaee1a1c16f8706
KienDev <ngotrungkien2k2@gmail.com>
2023-11-04 22:37:52 +0700

    CRUD role

---
+++ OrderFood.BL/RoleBL/RoleBL.cs
@@ -0,0 +1,21 @@
+﻿using OrderFood.Common.Models;
+using OrderFood.DL;
+using OrderFood.DL.RoleDL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderFood.BL.RoleBL
+{
+    public class RoleBL : BaseBL<Role>, IRoleBL
+    {
+        private IRoleDL _roleDL;
+
+        public RoleBL(IRoleDL roleDL) : base(roleDL)
+        {
+            _roleDL = roleDL;
+        }
+    }
+}
---
+++ OrderFood.DL/RoleDL/RoleDL.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using OrderFood.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderFood.DL.RoleDL
+{
+    public class RoleDL : BaseDL<Role>, IRoleDL
+    {
+        public RoleDL(IConfiguration configuration) : base(configuration)
+        {
+        }
+    }
+}
